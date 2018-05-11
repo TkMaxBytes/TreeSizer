14482d51a5f99c04ea7221e24c8a9cccef56f609
TKMax <terrenceknoesen@gmail.com>
2018-05-11 15:52:19 +0100

    Have added a Utils class to testing class for getting paths to testing resources.

--- TreeSizer.UnitTest/UnitTests.cs
+++ TreeSizer.UnitTest/UnitTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NUnit.Framework;
 using System.IO;
+using System.Reflection;
 using System.Diagnostics;
 using TreeSizer;
 
@@ -9,6 +10,48 @@ namespace TreeSizer.UnitTest
     [TestFixture]
     public class TreeListProcessorTests
     {
+        #region Constants
+
+        #endregion //================================================
+
+        #region Enums
+
+        #endregion //================================================
+
+        #region Variables
+        private string mstrDataDir = null;
+        #endregion //================================================
+
+        #region Constructors
+
+        #endregion //================================================
+
+        #region Properties
+
+        #endregion //================================================
+
+        #region Methods
+
+        #endregion //================================================
+
+        #region Events
+
+        #endregion //================================================
+
+
+
+        [OneTimeSetUp]
+        public void SetupDirs()
+        {
+            string strAppName = Utils.GetApplicationName(0);
+            string strDirPath = Utils.AssemblyDirectory();
+            string strDataPath = strDirPath.Substring(0, strDirPath.IndexOf(strAppName));
+            strDataPath = Path.Combine(strDataPath, strAppName);
+            strDataPath = Path.Combine(strDataPath, "data");
+            strDataPath = Path.Combine(strDataPath, "test");
+            mstrDataDir = strDataPath;
+        }
+
         [Test]
         public void CTOR_CMTreeListProcessor_NoPathForFileInfo()
         {
@@ -33,13 +76,58 @@ namespace TreeSizer.UnitTest
         [Test]
         public void CTOR_CMTreeListProcessor_BlankFile()
         {
+
+            /**Terrence Knoesen 
+             * Get the path to the BlankDoc.txt as this is used for this test
+            **/
+            string strFileName = Path.Combine(mstrDataDir, "BlankDoc.txt");
+            
             /**Terrence Knoesen 
              * Check that an ArgumentNullException is thrown for this scenario.
             **/
-            var ex = Assert.Throws<IOException>(() => new CMDTreeListProcessor(new System.IO.FileInfo(@"..\..\data\test\BlankDoc.txt")));
-            //Assert.That(ex.Message, Is.EqualTo("The TreeListFile doesn't exist!\n\nFile is 'c:\\Random\\TestFile.txt'."));
+            var ex = Assert.Throws<ApplicationException>(() => new CMDTreeListProcessor(new System.IO.FileInfo(strFileName)));
+            string strExpectedMessage = "The TreeListFile '" + strFileName + "' \n\nIs zero bytes in length!";
+            Assert.That(ex.Message, Is.EqualTo(strExpectedMessage));
             //Debug.WriteLine(ex.Message);
+        }
+    }
+
+
+
+
+    static class Utils
+    {
+
+        public static string AssemblyDirectory()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return Path.GetDirectoryName(path);
+        }
+
+        public static string GetApplicationName(int intReturnType)
+        {
+            string strVer;
+
+            Assembly asm = Assembly.GetExecutingAssembly();
+            AssemblyName asmName = asm.GetName();
+            Version objVer = asmName.Version;
+
+
+            string strName = asmName.Name;
+            switch (intReturnType)
+            {
+                case 0:
+                    return strName;
+                case 1:
+                    strVer = String.Format("{0} - {1}.{2}.{3}.{4}", strName, objVer.Major, objVer.Minor, objVer.Build, objVer.Revision);
+                    return strVer;
+                default:
+                    return strName;
+            }
         }
 
     }
+
 }
