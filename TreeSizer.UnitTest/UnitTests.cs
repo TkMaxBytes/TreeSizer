0b65ee2f21b65c88d13c5c7de4a08fca23a751ef
TKMax <terrenceknoesen@gmail.com>
2018-05-11 09:29:35 +0100

    Unit Test updates.

---
+++ TreeSizer.UnitTest/UnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using TreeSizer;
+
+namespace TreeSizer.UnitTest
+{
+    [TestFixture]
+    public class TreeListProcessorTests
+    {
+        [Test]
+        public void CTOR_CMTreeListProcessor_NoPathForFileInfo()
+        {
+            CMDTreeListProcessor objTLP = null;
+
+            objTLP = new CMDTreeListProcessor(new System.IO.FileInfo(null));
+        }
+    }
+}
